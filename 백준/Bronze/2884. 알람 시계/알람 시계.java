e7dd90b86c26905a3a5866dcc973c31b9d36e21c
다인 <129330821+dainnida@users.noreply.github.com>
2024-06-19 21:36:08 +0900

    [Bronze III] Title: 알람 시계, Time: 200 ms, Memory: 18500 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+		int hour = sc.nextInt();
+		int min = sc.nextInt();
+        
+        if(min >= 45){
+            min -= 45;
+            System.out.println(hour+ " "+ min);
+            return;
+        }
+        if(hour == 0)
+            hour = 23;
+        else
+            hour--;
+        min = min + 60 - 45;
+        
+        System.out.println(hour+ " "+ min);
+    }
+}

2c88bb1e650b9d4149b9390af0864e6fb57f030a
다인 <129330821+dainnida@users.noreply.github.com>
2024-07-19 17:04:18 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+score = int(input())
+
+# 파이썬은 switch문이 없군!
+if score>=90:
+    print("A")
+elif score>=80:
+    print("B")
+elif score>=70:
+    print("C")
+elif score>=60:
+    print("D")
+else:
+    print("F")

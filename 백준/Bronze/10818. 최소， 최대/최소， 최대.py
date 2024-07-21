5a9beb437f8963b7d9f742766b7026a0cdc9f155
다인 <129330821+dainnida@users.noreply.github.com>
2024-07-21 23:38:08 +0900

    [Bronze III] Title: 최소, 최대, Time: 376 ms, Memory: 153832 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,12 +1,13 @@
 N = int(input())
-max = -1000001
-min = 1000001
+# max = -1000001
+# min = 1000001
 arr = list(map(int, input().split()))
 
-for i in arr:
-    if i>max:
-        max=i
-    if i<min:
-        min=i
+# for i in arr:
+#     if i>max:
+#         max=i
+#     if i<min:
+#         min=i
+# print(min, max)
 
-print(min, max)
+print(min(arr), max(arr))

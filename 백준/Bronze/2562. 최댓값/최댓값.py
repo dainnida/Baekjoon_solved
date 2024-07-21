f04a8a8038d9772e1fd0693796862f68f9c36de8
다인 <129330821+dainnida@users.noreply.github.com>
2024-07-21 23:47:02 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,10 +1,18 @@
-max = 0
-index = 0
+# max = 0
+# index = 0
+
+# for i in range(9):
+#     num = int(input())
+#     if num>max:
+#         max=num
+#         index = i+1
+
+# print(max, index, sep="\n")
+
+arr = []
 
 for i in range(9):
-    num = int(input())
-    if num>max:
-        max=num
-        index = i+1
+    arr.append(int(input()))
 
-print(max, index, sep="\n")
+print(max(arr))
+print(arr.index(max(arr))+1)

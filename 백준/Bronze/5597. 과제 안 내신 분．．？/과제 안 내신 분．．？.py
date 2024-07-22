fcf894c116771476f6ddae9a144390b23e6415df
다인 <129330821+dainnida@users.noreply.github.com>
2024-07-22 14:33:22 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,5 +1,8 @@
-arr = [int(input()) for _ in range(28)]
+arr = [i for i in range(1, 31)]
 
-for i in range(1, 31):
-    if i not in arr:
-        print(i)
+for i in range(28):
+    num = int(input())
+    arr.remove(num)
+
+print(arr[0])
+print(arr[1])

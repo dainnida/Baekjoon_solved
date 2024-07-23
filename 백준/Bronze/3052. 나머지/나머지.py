c39b369d91344a53d073c53002b435369cfc134e
다인 <129330821+dainnida@users.noreply.github.com>
2024-07-23 15:52:01 +0900

    [Bronze II] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -2,7 +2,8 @@ arr = []
 
 for i in range(10):
     num = int(input()) % 42
-    if num not in arr:
-        arr.append(num)
-
-print(len(arr))
+    # if num not in arr:
+    #    arr.append(num)
+    arr.append(num)
+# print(len(arr))
+print(len(set(arr)))

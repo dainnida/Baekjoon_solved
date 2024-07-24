d95c41d44256b8ad2d2e1dc7ecb62e219cd26fa7
다인 <129330821+dainnida@users.noreply.github.com>
2024-07-24 21:30:10 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,11 +1,10 @@
-import math
-
 N, M = map(int, input().split())
 arr = [i+1 for i in range(N)]
 
 for i in range(M):
     a, b = map(int, input().split())
-    for i in range(a, math.ceil((a+b)/2)):
-        arr[i-1], arr[a+b-i-1] = arr[a+b-i-1], arr[i-1]
+    temp = arr[a-1:b]
+    temp.reverse()
+    arr[a-1:b] = temp
 
 print(*arr)

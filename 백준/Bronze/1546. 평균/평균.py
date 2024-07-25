3b1acbf7bc28276430efd57138212ad20d33d7d0
다인 <129330821+dainnida@users.noreply.github.com>
2024-07-25 13:40:24 +0900

    [Bronze I] Title: 평균, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,4 +1,9 @@
 N = int(input())
 arr = list(map(int, input().split()))
+new_arr = []
+max = max(arr)
+
+for i in range(N):
+    new_arr.append(arr[i] / max * 100)
     
-print(sum(arr) / N / max(arr) * 100)
+print(sum(new_arr) / N)

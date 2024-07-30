722f128c7826081214509d670c81c1874acf0f35
다인 <129330821+dainnida@users.noreply.github.com>
2024-07-30 19:43:53 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -2,7 +2,8 @@ S = input()
 alp = "abcdefghijklmnopqrstuvwxyz"
 
 for i in alp:
-    if i in S:
-        print(S.index(i), end=' ')
-    else:
-        print(-1, end=' ')
+    # if i in S:
+    #     print(S.index(i), end=' ')
+    # else:
+    #     print(-1, end=' ')
+    print(S.find(i), end=' ')

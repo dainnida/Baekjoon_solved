e6084b220d06d0c3392ffcd8f8c830d057d4d66f
다인 <129330821+dainnida@users.noreply.github.com>
2024-08-01 16:12:43 +0900

    [Bronze II] Title: 상수, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,7 +1,11 @@
-A, B = map(list, input().split())
-A.reverse()
-A = ''.join(A)
-B.reverse()
-B = ''.join(B)
+# A, B = map(list, input().split())
+# A.reverse()
+# A = ''.join(A)
+# B.reverse()
+# B = ''.join(B)
+
+A, B = input().split()
+A = A[::-1]
+B = B[::-1]
 
 print(max(A, B))

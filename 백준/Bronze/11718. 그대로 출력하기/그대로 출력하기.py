8711187e17dc721aa9e810ce9e6c8dc0ccb3be8d
다인 <129330821+dainnida@users.noreply.github.com>
2024-08-03 13:36:35 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -1,6 +1,11 @@
-while True:
-    try:
-        str = input()
-        print(str)
-    except EOFError:
-        break
+# while True:
+#     try:
+#         str = input()
+#         print(str)
+#     except EOFError:
+#         break
+import sys
+
+s = sys.stdin.readline()
+for i in s:
+    print(i.rstrip())

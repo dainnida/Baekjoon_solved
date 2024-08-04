d5cacbc9a6397aba8391e810107bee0942e7f97b
다인 <129330821+dainnida@users.noreply.github.com>
2024-08-04 21:54:46 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,7 +1,6 @@
 num = int(input())
 
-for i in range(1, 2*num):
-    if i<=num:
-        print(" "*(num-i) + "*"*(2*i-1))
-    else:
-        print(" "*(i-num) + "*"*(2*(2*num-i)-1))
+for i in range(1, num):
+    print(" "*(num-i) + "*"*(2*i-1))
+for i in range(num, 0, -1):
+    print(" "*(num-i) + "*"*(2*i-1))

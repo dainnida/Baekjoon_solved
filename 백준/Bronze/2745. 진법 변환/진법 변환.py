8ef95db1627703648a8407a2f607092eb9c00e06
다인 <129330821+dainnida@users.noreply.github.com>
2024-08-13 14:36:14 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,3 +1,13 @@
+# alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# N, B = input().split()
+# B = int(B)
+# sum = 0
+
+# for i in range(len(N)):
+#     sum += (B ** i) * alphabet.index(N[len(N)-i-1])
+
+# print(sum)
+
 N, B = input().split(" ")
 num_list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 result = 0

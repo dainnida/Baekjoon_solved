e5b4a136fb4d47d524790bd500f79abddf9338b1
다인 <129330821+dainnida@users.noreply.github.com>
2024-08-13 14:49:16 +0900

    [Bronze I] Title: 진법 변환 2, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -1,3 +1,14 @@
+# alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# N, B = map(int, input().split())
+# num = []
+
+# while N>0:
+#     num.append(N % B)
+#     N = N // B
+
+# for i in num[::-1]:
+#     print(alphabet[i], end='')
+
 N, B = map(int, input().split(" "))
 num_list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 result = ''

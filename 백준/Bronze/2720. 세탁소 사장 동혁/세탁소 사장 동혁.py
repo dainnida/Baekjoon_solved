b437a3b141b7cf0841c9c2ad741f99c387978553
다인 <129330821+dainnida@users.noreply.github.com>
2024-08-14 14:56:59 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,17 +1,8 @@
 num = int(input())
-quarter = 25
-dime = 10
-nickel = 5
-penny = 1
 
 for _ in range(num):
     price = int(input())
-    
-    print(price // quarter, end=' ')
-    price %= quarter
-    print(price // dime, end= ' ')
-    price %= dime
-    print(price // nickel, end=' ')
-    price %= nickel
-    print(price // penny)
-    price %= penny
+    for i in [25, 10, 5, 1]:
+        print(price // i, end=' ')
+        price %= i
+    print()

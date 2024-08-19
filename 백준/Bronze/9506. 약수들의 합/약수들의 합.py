8f91c8cd98e0876f82b99e2d56ce4d8db490eb63
다인 <129330821+dainnida@users.noreply.github.com>
2024-08-19 15:16:59 +0900

    [Bronze I] Title: 약수들의 합, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -7,8 +7,10 @@ while n != -1:
             aliquot.append(i)
 
     if sum(aliquot) == n:
-        print(n, "=", end=' ')
-        print(*aliquot, sep=' + ')
+        # print(n, "=", end=' ')
+        # print(*aliquot, sep=' + ')
+        # print(n, "=", " + ".join(map(str, aliquot)))
+        print(n, "=", " + ".join(str(i) for i in aliquot))
     else:
         print(n, "is NOT perfect.")
 

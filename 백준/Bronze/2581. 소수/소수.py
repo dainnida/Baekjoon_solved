21c3ee20850a12984c9d283a29011a43e129fa23
다인 <129330821+dainnida@users.noreply.github.com>
2024-08-20 22:35:39 +0900

    [Bronze II] Title: 소수, Time: 264 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,14 +1,16 @@
 N = int(input())
 M = int(input())
-prime = list(range(N, M+1))
+prime = []
 
 for num in range(N, M+1):
-    if num == 1:
-        prime.remove(num)
-    for i in range(2, num//2 + 1):
-        if num % i == 0:
-            prime.remove(num)
-            break
+    is_prime = True
+    if num > 1:
+        for i in range(2, num//2 + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime == True:
+            prime.append(num)
 
 if prime:
     print(sum(prime))

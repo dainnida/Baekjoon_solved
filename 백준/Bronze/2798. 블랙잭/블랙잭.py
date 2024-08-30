cc429969c482d601b3917d357415343b0037545f
다인 <129330821+dainnida@users.noreply.github.com>
2024-08-30 23:56:49 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,12 +1,11 @@
+from itertools import combinations
+
 N, M = map(int, input().split())
 num = list(map(int, input().split()))
-max = 0
+result = 0
 
-for i in range(N-2):
-    for j in range(i+1, N-1):
-        for k in range(j+1, N):
-            sum = num[i] + num[j] + num[k]
-            if sum <= M and sum > max:
-                max = sum
+for three in combinations(num, 3):
+    if sum(three) <= M and sum(three) > result:
+        result = sum(three)
 
-print(max)
+print(result)

e25ae4fecd258f8f592737cecde49be26588809b
다인 <129330821+dainnida@users.noreply.github.com>
2024-09-05 01:27:50 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+board = []
+result = 64
+
+for i in range(N):
+    board.append(list(input()))
+
+for i in range(N-7):
+    for j in range(M-7):
+        countW = 0 # 좌상단이 W인 경우
+        countB = 0 # 좌상단이 B인 경우
+        first = board[i][j]
+        for a in range(i, i+8):
+            for b in range(j, j+8):
+                if (a + b) % 2 == 0:
+                # if (i%2 == a%2 and j%2 == b%2) or (i%2 != a%2 and j%2 != b%2):
+                    if board[a][b] != 'W':
+                        countW += 1
+                    if board[a][b] != 'B':
+                        countB += 1
+                else:
+                    if board[a][b] == 'W':
+                        countW += 1
+                    if board[a][b] == 'B':
+                        countB += 1
+        result = min(result, countW, countB)
+
+print(result)

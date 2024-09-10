69377a6d440f7fc289e7b293c6210cf7fb1059b6
다인 <129330821+dainnida@users.noreply.github.com>
2024-09-10 20:11:41 +0900

    [Silver V] Title: 단어 정렬, Time: 3536 ms, Memory: 34500 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,8 +1,10 @@
+import sys
+
 N = int(input())
 words = []
 
 for _ in range(N):
-    word = input()
+    word = sys.stdin.readline().strip()
     if word not in words:
         words.append(word)
 

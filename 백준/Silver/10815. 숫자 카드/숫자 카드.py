04e5831196c414909130c81cd66499a20e196e29
다인 <129330821+dainnida@users.noreply.github.com>
2024-09-14 00:04:06 +0900

    [Silver V] Title: 숫자 카드, Time: 1716 ms, Memory: 113332 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -5,13 +5,20 @@ cards = list(map(int, sys.stdin.readline().split()))
 M = int(input())
 finds = list(map(int, sys.stdin.readline().split()))
 
-dic = {}
+cards.sort()
+
+def binary_search(target):
+    low, high = 0, N-1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if target == cards[mid]:
+            return 1
+        elif target > cards[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return 0
+
 for i in finds:
-    dic[i] = 0
-
-for i in cards:
-    if i in dic:
-        dic[i] = 1
-
-for i in dic:
-        print(dic[i], end=' ')
+    print(binary_search(i), end=' ')

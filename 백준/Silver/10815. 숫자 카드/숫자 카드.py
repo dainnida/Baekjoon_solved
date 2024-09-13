d910876249b6c2b979174a7620a9f19492945b2a
다인 <129330821+dainnida@users.noreply.github.com>
2024-09-13 23:32:36 +0900

    [Silver V] Title: 숫자 카드, Time: 620 ms, Memory: 115372 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,12 +1,16 @@
 import sys
 
 N = int(input())
-cards = set(map(int, sys.stdin.readline().split()))
+cards = list(map(int, sys.stdin.readline().split()))
 M = int(input())
-
 finds = list(map(int, sys.stdin.readline().split()))
+
+dic = {}
+for i in cards:
+    dic[i] = 1
+
 for i in finds:
-    if i in cards:
+    if i in dic:
         print(1, end=' ')
     else:
-        print(0, end=' ')
+         print(0, end=' ')

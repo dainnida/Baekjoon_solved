f334654f06e005391c5d87da327e993ff7384f82
다인 <129330821+dainnida@users.noreply.github.com>
2024-09-17 13:29:57 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 188 ms, Memory: 46916 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -6,11 +6,11 @@ pockemon = {}
 for i in range(N):
     pockemon[sys.stdin.readline().strip()] = i+1
 
-reversedPokemon = dict(map(reversed, pockemon.items()))
+reversedPokemon = list(pockemon.keys())
 
 for _ in range(M):
     find = sys.stdin.readline().strip()
     if pockemon.get(find) != None:    # 입력값이 알파벳이다.
         print(pockemon[find])
     else:
-        print(reversedPokemon[int(find)])
+        print(reversedPokemon[int(find)-1])

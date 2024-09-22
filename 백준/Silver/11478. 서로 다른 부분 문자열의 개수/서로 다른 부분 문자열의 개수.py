a17eac4fb5450d368b8141f0a8f553faed2ce398
다인 <129330821+dainnida@users.noreply.github.com>
2024-09-22 21:57:03 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 300 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -5,7 +5,7 @@ for i in range(1, len(S)+1): # 슬라이싱할 원소 개수
     s = set()
     j = 0
     while j+i <= len(S):
-        s.add(S[j:j+i])
+        s.add(S[j:j+i]) # i개수만큼 슬라이싱
         j += 1
     result += len(s)
 

bbaed6c057b9a1d03a14f3a40da9e381953dd535
다인 <129330821+dainnida@users.noreply.github.com>
2024-09-23 13:52:13 +0900

    [Bronze I] Title: 최소공배수, Time: 32 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,9 +1,9 @@
-import math
+import sys, math
 
 T = int(input())
 
 for _ in range(T):
-    A, B = map(int, input().split())
+    A, B = map(int, sys.stdin.readline().split())
     # if A>B:
     #     A, B = B, A
     # for i in range(A, 0, -1):

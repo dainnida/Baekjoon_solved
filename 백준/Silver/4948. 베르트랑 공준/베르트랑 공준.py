25e34328015dee9d226ea4d95dbee1ad2e5a12da
다인 <129330821+dainnida@users.noreply.github.com>
2024-09-30 23:41:58 +0900

    [Silver II] Title: 베르트랑 공준, Time: 952 ms, Memory: 36316 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,20 +1,21 @@
-import sys
+import sys, math
 
-prime = {}
-prime[1] = False
+def isPrime(num):
+    for i in range(2, int(num**(1/2))+1): # math.sqrt(n)
+        if num % i == 0:
+            return False
+    return True
+
+prime = set()
 for i in range(2, 123456*2+1):
-    prime[i] = True
-    for j in range(2, int(i**(1/2))+1):
-        if i % j == 0:
-            prime[i] = False
-            break
+    if isPrime(i):
+        prime.add(i)
 
 n = int(sys.stdin.readline())
-
 while n != 0:
     cnt = 0
     for i in range(n+1, 2*n+1):
-        if prime[i]:
+        if i in prime:
             cnt += 1
     print(cnt)
     n = int(sys.stdin.readline())

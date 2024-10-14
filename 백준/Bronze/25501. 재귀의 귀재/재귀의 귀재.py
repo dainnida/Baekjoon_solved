8fde2a045026ede4d5a03342e26a8af4eecd0a7f
다인 <129330821+dainnida@users.noreply.github.com>
2024-10-14 16:38:48 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 308 ms, Memory: 31180 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -3,20 +3,18 @@ input = sys.stdin.readline
 
 T = int(input())
 
-def recursion(s, l, r):
-    global count
+def recursion(s, l, r, count):
     count += 1
     if l >= r:
-        return 1
+        return 1, count
     elif s[l] != s[r]:
-        return 0
+        return 0, count
     else:
-
-        return recursion(s, l+1, r-1)
+        return recursion(s, l+1, r-1, count)
 
 def isPalindrome(s):
-    return recursion(s, 0, len(s)-1)
+    count = 0
+    return recursion(s, 0, len(s)-1, count)
 
 for _ in range(T):
-    count = 0
-    print(isPalindrome(input().strip()), count)
+    print(*isPalindrome(input().strip()))

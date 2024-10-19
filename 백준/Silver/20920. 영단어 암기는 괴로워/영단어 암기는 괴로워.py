ad32c3b51f10d50e94a7fcb1e510f61686cc4a46
다인 <129330821+dainnida@users.noreply.github.com>
2024-10-19 23:45:48 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 300 ms, Memory: 60036 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+dic = dict()
+note = []
+
+for _ in range(N):
+    word = input().strip()
+    if len(word) >= M:
+        if word in dic:
+            dic[word] += 1 
+        else:
+            dic[word] = 1
+dic = dict(sorted(dic.items())) # 알파벳 순으로 정렬
+
+dic = dict(sorted(dic.items(), key = lambda x : (-x[1], -len(x[0]))))
+print(*dic, sep='\n')

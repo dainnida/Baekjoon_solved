6b3a0b08308ff4729a975272a51fb8a9ad746284
다인 <129330821+dainnida@users.noreply.github.com>
2024-10-20 23:25:51 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 2212 ms, Memory: 91224 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
@@ -0,0 +1,49 @@
+import sys
+input = sys.stdin.readline
+
+def merge_sort(A, p, r): # A[p..r]을 오름차순 정렬한다.
+    if p < r:
+        q = (p+r)//2 # q는 p, r의 중간 지점
+        merge_sort(A, p, q) # 전반부 정렬
+        merge_sort(A, q+1, r) # 후반부 정렬
+        merge(A, p, q, r) # 병합
+
+# A[p..q]와 A[q+1..r]을 병합하여 A[p..r]을 오름차순 정렬된 상태로 만든다.
+# A[p..q]와 A[q+1..r]은 이미 오름차순으로 정렬되어 있다.
+def merge(A, p, q, r):
+    i = p
+    j = q+1
+    tmp = []
+    global count, result
+    
+    while i<=q and j<=r:
+        if A[i] <= A[j]:
+            tmp.append(A[i])
+            i += 1
+        else:
+            tmp.append(A[j])
+            j += 1
+    while i <= q: # 왼쪽 배열 부분이 남은 경우
+        tmp.append(A[i])
+        i += 1
+    while j <= r: # 오른쪽 배열 부분이 남은 경우
+        tmp.append(A[j])
+        j += 1
+    
+    i = p
+    t = 0
+    while i <= r: # 결과를 A[p..r]에 저장
+        A[i] = tmp[t]
+        count += 1
+        if count == K:
+            result = A[i]
+            break
+        i += 1
+        t += 1
+
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+count = 0
+result = -1
+merge_sort(A, 0, N-1)
+print(result)

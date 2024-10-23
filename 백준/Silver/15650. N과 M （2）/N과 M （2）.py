9fc21e8734ebb151afeb22b6756b4bcafc5cdd1f
다인 <129330821+dainnida@users.noreply.github.com>
2024-10-23 22:54:01 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,9 +1,18 @@
-import sys, itertools
+import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-lst = [i+1 for i in range(N)]
+lst = []
 
-nCr = itertools.combinations(lst, M)
-for i in nCr:
-    print(*i)
+def backtracking(i=1):
+    if len(lst) == M:
+        print(*lst)
+        return
+    
+    for j in range(i, N+1):
+        if j not in lst:
+            lst.append(j)
+            backtracking(j+1)
+            lst.pop()
+
+backtracking()

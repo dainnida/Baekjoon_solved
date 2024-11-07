c6bcbec603c8fad58c405ef58b4ba1ac445f9ab4
다인 <129330821+dainnida@users.noreply.github.com>
2024-11-07 14:49:50 +0900

    [Silver III] Title: 수열, Time: 76 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -3,14 +3,9 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 temp = list(map(int, input().split()))
-seq = [0] # temp의 0인덱스부터 n인덱스까지의 합을 n+1인덱스에 저장
+seq = [sum(temp[:K])]
 
-for i in range(1, N+1):
-    seq.append(seq[i-1]+temp[i-1]) # temp는 인덱스 0부터 저장됨
+for i in range(K, N):
+    seq.append(seq[i-K] - temp[i-K] + temp[i])
 
-res = seq[K]
-
-for i in range(K, N+1):
-    tmp = seq[i]-seq[i-K]
-    res = max(tmp, res)
-print(res)
+print(max(seq))

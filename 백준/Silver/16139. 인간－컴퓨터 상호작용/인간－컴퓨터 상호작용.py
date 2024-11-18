8337481d4a4724e782c76794efc5bb690d7cae86
다인 <129330821+dainnida@users.noreply.github.com>
2024-11-18 20:32:04 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 684 ms, Memory: 93316 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -3,13 +3,12 @@ input = sys.stdin.readline
 
 S = input().strip()
 q = int(input())
-cnt = [[0] * 26 for _ in range(len(S))]
+cnt = [[0] * 26]
 
 cnt[0][ord(S[0])-97] = 1
 for i in range(1, len(S)):
-    cnt[i][ord(S[i])-97] = 1
-    for j in range(26):
-        cnt[i][j] += cnt[i-1][j]
+    cnt.append(cnt[i-1][:])
+    cnt[i][ord(S[i])-97] += 1
 
 for _ in range(q):
     lst = input().split()

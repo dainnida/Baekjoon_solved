7ad254e899bb7f55364301ecf2df08a59f2ef7e2
다인 <129330821+dainnida@users.noreply.github.com>
2025-11-15 21:05:02 +0900

    [level 2] Title: 전화번호 목록, Time: 101.88 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,7 @@
 def solution(phone_book):
-    hash_map = {}
-    for nums in phone_book:
-        hash_map[nums]=1
-        
-    for nums in phone_book:
-        arr = "" 
-        for num in nums: 
-            arr += num
+    phone_book.sort()
     
-            if arr in hash_map and arr != nums:       
-                return False 
+    for i in range(len(phone_book)-1):
+        if phone_book[i]==phone_book[i+1][:len(phone_book[i])]:
+            return False
     return True

b2416b8ceb5ac6f7f0b178ad936f4210d615a77d
다인 <129330821+dainnida@users.noreply.github.com>
2025-12-05 18:30:16 +0900

    [level 2] Title: 프로세스, Time: 1.30 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,14 +2,16 @@ from collections import deque
 
 def solution(priorities, location):
     queue = deque((value, i) for i, value in enumerate(priorities))
-    answer = []
+    answer = 0
     
     while queue:
-        maxTuple = max(queue, key = lambda x: x[0])
+        # maxTuple = max(queue, key = lambda x: x[0])
         process = queue.popleft()
-        if process[0] < maxTuple[0]:
+        if queue and process[0] < max(queue)[0]:
             queue.append(process)
         else:
-            answer.append(process[1])
+            answer += 1
+            if process[1] == location:
+                break
             
-    return answer.index(location)+1
+    return answer

ada7078ec3e400c65baceff43558b527d22d10fd
다인 <129330821+dainnida@users.noreply.github.com>
2026-02-07 13:38:13 +0900

    [level 0] Title: ad 제거하기, Time: 0.71 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -4,9 +4,8 @@ class Solution {
     public List<String> solution(String[] strArr) {
         List<String> arrList = new ArrayList<>(strArr.length);
         for (String s: strArr){
-            if (s.contains("ad"))
-                continue;
-            arrList.add(s);
+            if (!s.contains("ad"))
+                arrList.add(s);
         }
         return arrList;
     }

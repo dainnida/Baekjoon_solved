935aa8be90433e34006150cd6b24fce79a4ebc31
다인 <129330821+dainnida@users.noreply.github.com>
2026-02-13 17:08:31 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 5.45 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,5 +1,13 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
+        // for (int[] query: queries) {
+        //     String part = my_string.substring(query[0], query[1]+1);
+        //     String reversed = new StringBuffer(part).reverse().toString();
+        //     my_string = my_string.substring(0, query[0]) + reversed + my_string.substring(query[1]+1);
+        // }
+        // return my_string;
+        
+        // StringBuilder의 replace 사용
         StringBuilder sb = new StringBuilder(my_string);
         for (int[] query: queries) {
             String part = sb.substring(query[0], query[1]+1);

a8f8fcf9b5e30ef4dbe074639b51e1e936f19dbb
다인 <129330821+dainnida@users.noreply.github.com>
2026-02-17 01:46:48 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.49 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int n, int k) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i=k; i<=n; i+=k)
+            answer.add(i);
+        return answer;
+    }
+}

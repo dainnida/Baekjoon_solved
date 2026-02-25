e18972bbfd37e27410eeebf5817d9f6ee899625f
다인 <129330821+dainnida@users.noreply.github.com>
2026-02-26 01:29:39 +0900

    [level 2] Title: 피보나치 수, Time: 138.57 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,5 +1,12 @@
-def solution(n):
-    fibo = [0, 1]
+def fib(n):
+    a, b = 0, 1
     for i in range(2, n+1):
-        fibo.append(fibo[i-1] + fibo[i-2])
-    return fibo[n] % 1234567
+        a, b = b, a+b
+    return b
+
+def solution(n):
+    # fibo = [0, 1]
+    # for i in range(2, n+1):
+    #     fibo.append(fibo[i-1] + fibo[i-2])
+    # return fibo[n] % 1234567
+    return fib(n) % 1234567

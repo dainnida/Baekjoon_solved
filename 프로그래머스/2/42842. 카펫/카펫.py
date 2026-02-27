3a7fef7b040439a661df510a6077ec31b6029c94
다인 <129330821+dainnida@users.noreply.github.com>
2026-02-27 18:04:14 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,13 +1,12 @@
 def solution(brown, yellow):
-    for y in range(1, int(yellow**0.5)+1):
-        if yellow % y == 0:
-            x = yellow / y
-            if (x+2) * (y+2) == brown + yellow:
-                return [x+2, y+2]
+    # for y in range(1, int(yellow**0.5)+1):
+    #     if yellow % y == 0:
+    #         x = yellow / y
+    #         if (x+2) * (y+2) == brown + yellow:
+    #             return [x+2, y+2]
     
-    # # (x+2)(y+2) = brown + yellow
-    # total = (brown-4) / 2 # yellow_width(x) + yellow_height(y)
-    # product = yellow # yellow_width(x) * yellow_height(y)
-    # x = int(total + (total**2 - 4*product)**0.5) / 2
-    # y = total - x
-    # return [x+2, y+2]
+    total = (brown-4) / 2 # yellow_width(x) + yellow_height(y)
+    product = yellow # yellow_width(x) * yellow_height(y)
+    x = int(total + (total**2 - 4*product)**0.5) / 2
+    y = total - x
+    return [x+2, y+2]

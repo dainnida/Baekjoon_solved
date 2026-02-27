10972c1cb79071e8d3028aceb0163b8fea275d21
다인 <129330821+dainnida@users.noreply.github.com>
2026-02-27 16:55:15 +0900

    [level 2] Title: 짝지어 제거하기, Time: 89.46 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    stack = []
+    
+    for ch in s:
+        if stack and stack[-1] == ch:
+            stack.pop()
+        else:
+            stack.append(ch)
+    if stack:
+        return 0
+    return 1

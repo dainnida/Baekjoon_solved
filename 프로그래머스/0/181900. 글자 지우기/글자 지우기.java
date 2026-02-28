d527a4d2ae63681aa587f42c1176fd97c7a7cd19
다인 <129330821+dainnida@users.noreply.github.com>
2026-02-28 19:44:02 +0900

    [level 0] Title: 글자 지우기, Time: 0.58 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -3,12 +3,17 @@ import java.util.*;
 class Solution {
     public String solution(String my_string, int[] indices) {
         String answer = "";
-        for (int i=0; i<my_string.length(); i++) {
-            int finalI = i;
-            if (Arrays.stream(indices).anyMatch(x -> x == finalI))
-                continue;
-            answer += my_string.charAt(i);
-        }
-        return answer;
+        // for (int i=0; i<my_string.length(); i++) {
+        //     int finalI = i;
+        //     if (Arrays.stream(indices).anyMatch(x -> x == finalI))
+        //         continue;
+        //     answer += my_string.charAt(i);
+        // }
+        String tmp[] = my_string.split("");
+        for (int i: indices) 
+            tmp[i] = "";
+        
+        // return answer;
+        return String.join("", tmp);
     }
 }

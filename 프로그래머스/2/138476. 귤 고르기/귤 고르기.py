a436181977374b56fda93d2f5873b3995cd1c244
다인 <129330821+dainnida@users.noreply.github.com>
2026-02-28 11:54:23 +0900

    [level 2] Title: 귤 고르기, Time: 20.99 ms, Memory: 21 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,9 +1,12 @@
+from collections import Counter
+
 def solution(k, tangerine):
     answer = 0
-    dict = {}
-    for t in tangerine:
-        dict[t] = dict.get(t, 0) + 1
-    cnt = sorted(dict.values(), reverse = True) # 개수만 담겨 있는 리스트
+    # dict = {}
+    # for t in tangerine:
+    #     dict[t] = dict.get(t, 0) + 1
+    counts = Counter(tangerine)
+    cnt = sorted(counts.values(), reverse = True) # 개수만 담겨 있는 리스트
     
     for i in cnt:
         if k <= 0:

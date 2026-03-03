3b79fa207dae82996cb6030ffb8c1f1ff6b9a1c0
다인 <129330821+dainnida@users.noreply.github.com>
2026-03-03 21:49:13 +0900

    [level 2] Title: 할인 행사, Time: 177.38 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def solution(want, number, discount):
     answer = 0
     dic = {}
@@ -5,11 +7,13 @@ def solution(want, number, discount):
         dic[want[i]] = number[i]
         
     for i in range(len(discount)-9):
-        tmp = dic.copy()
-        for item in discount[i:i+10]:
-            if item in want and tmp[item] > 0:
-                tmp[item] -= 1
-        if all(v==0 for v in tmp.values()):
+        # tmp = dic.copy()
+        # for item in discount[i:i+10]:
+        #     if item in want and tmp[item] > 0:
+        #         tmp[item] -= 1
+        # if all(v==0 for v in tmp.values()):
+        #     answer += 1
+        if dic == Counter(discount[i:i+10]):
             answer += 1
         
     return answer

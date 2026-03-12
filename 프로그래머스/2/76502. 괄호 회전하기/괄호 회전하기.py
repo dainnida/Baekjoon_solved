b61c08f6a101baab5b0fef0d62ee9f5d81f17782
다인 <129330821+dainnida@users.noreply.github.com>
2026-03-12 22:24:41 +0900

    [level 2] Title: 괄호 회전하기, Time: 190.98 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,27 @@
+def solution(s):
+    answer = 0
+    if len(s) % 2 != 0:
+        return 0
+    lngth = len(s)
+    s = s + s
+    for i in range(lngth):
+        stack = []
+        for j in range(i, i+lngth):
+            if s[j] == "(" or s[j] == "[" or s[j] == "{":
+                stack.append(s[j])
+            elif stack:
+                if ( (stack[-1] == "(" and s[j] == ")")
+                    or (stack[-1] == "[" and s[j] == "]")
+                    or (stack[-1] == "{" and s[j] == "}") ):
+                        stack.pop()
+                else: # 스택이 비어있지 않은데 짝이 맞지 않음
+                    stack.append(s[j])
+                    break
+            else: # 스택이 비어있는데 닫는 괄호가 들어옴
+                stack.append(s[j])
+                break
+                
+        if not stack:
+            answer += 1
+        
+    return answer

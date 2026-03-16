90dcd7c1cb379de4e2333943405fe5f774e67a7f
다인 <129330821+dainnida@users.noreply.github.com>
2026-03-16 22:58:21 +0900

    [level 2] Title: 피로도, Time: 50.06 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -0,0 +1,18 @@
+answer = 0
+visited = []
+
+def backtrack(k, dungeons, cnt):
+    global answer
+    answer = max(answer, cnt)
+    
+    for i in range(len(dungeons)):
+        if not visited[i] and k>=dungeons[i][0] and k-dungeons[i][1]>=0:
+            visited[i] = True
+            backtrack(k-dungeons[i][1], dungeons, cnt+1)
+            visited[i] = False
+
+def solution(k, dungeons):
+    global visited
+    visited = [False] * len(dungeons)
+    backtrack(k, dungeons, 0)
+    return answer

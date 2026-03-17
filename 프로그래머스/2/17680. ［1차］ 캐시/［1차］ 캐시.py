77d4ec75e0a3dfb6eab90ffe80a14b1fd14db0bd
다인 <129330821+dainnida@users.noreply.github.com>
2026-03-17 21:55:17 +0900

    [level 2] Title: [1차] 캐시, Time: 77.80 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    answer = 0
+    queue = deque()
+    if cacheSize == 0:
+        return 5*len(cities)
+    
+    for city in cities:
+        city = city.lower()
+        if city not in queue:
+            if len(queue) == cacheSize: # 캐시에 없는데 캐시 꽉 참
+                queue.popleft()
+            queue.append(city) # 캐시에 없지만 캐시 비어 있음
+            answer += 5
+        else: # 캐시에 있음 -> cache hit
+            queue.remove(city)
+            queue.append(city)
+            answer += 1
+    return answer

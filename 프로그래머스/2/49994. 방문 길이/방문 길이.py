56867e32e37fcbe307966ee85ce9f9d41a00e1b4
다인 <129330821+dainnida@users.noreply.github.com>
2026-03-23 19:47:03 +0900

    [level 2] Title: 방문 길이, Time: 1.32 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,22 +1,35 @@
 def solution(dirs):
-    visited_width = [[False]*10 for _ in range(11)]
-    visited_height = [[False]*10 for _ in range(11)]
-    move = {"U": [0, 1], "D": [0, -1], "R": [1, 0], "L": [-1, 0]}
-    where = [0, 0]
-    for dir in dirs:
-        if where[0]+move[dir][0] > 5 or where[0]+move[dir][0] < -5 \
-            or where[1]+move[dir][1] > 5 or where[1]+move[dir][1]  < -5:
-            continue
-        if dir in "DL":
-            where[0] += move[dir][0]
-            where[1] += move[dir][1]
-        if dir in "RL":
-            visited_width[where[1]+5][where[0]+5] = True
-        else:
-            visited_height[where[0]+5][where[1]+5] = True
-        if dir in "UR":
-            where[0] += move[dir][0]
-            where[1] += move[dir][1]
+#     visited_width = [[False]*10 for _ in range(11)]
+#     visited_height = [[False]*10 for _ in range(11)]
+#     move = {"U": [0, 1], "D": [0, -1], "R": [1, 0], "L": [-1, 0]}
+#     where = [0, 0]
+#     for dir in dirs:
+#         if where[0]+move[dir][0] > 5 or where[0]+move[dir][0] < -5 \
+#             or where[1]+move[dir][1] > 5 or where[1]+move[dir][1]  < -5:
+#             continue
+#         if dir in "DL":
+#             where[0] += move[dir][0]
+#             where[1] += move[dir][1]
+#         if dir in "RL":
+#             visited_width[where[1]+5][where[0]+5] = True
+#         else:
+#             visited_height[where[0]+5][where[1]+5] = True
+#         if dir in "UR":
+#             where[0] += move[dir][0]
+#             where[1] += move[dir][1]
 
-    return sum(row.count(True) for row in visited_width) \
-            + sum(column.count(True) for column in visited_height)
+#     return sum(row.count(True) for row in visited_width) \
+#             + sum(column.count(True) for column in visited_height)
+    path = set()
+    x, y = 0, 0
+    move = {"U": [0, 1], "D": [0, -1], "R": [1, 0], "L": [-1, 0]}
+    for dir in dirs:
+        nx, ny = x + move[dir][0], y + move[dir][1]
+        if not (-5 <= nx <= 5 and -5 <= ny <= 5):
+            continue
+        
+        path.add(tuple(sorted([(x, y), (nx, ny)])))
+        x, y = nx, ny
+    
+    return len(path)
+    

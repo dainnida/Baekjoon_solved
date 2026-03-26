6798d7b00117e14760d155fddc6a82a0b0adce30
다인 <129330821+dainnida@users.noreply.github.com>
2026-03-26 21:06:35 +0900

    [level 0] Title: 커피 심부름, Time: 1.34 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,12 +1,13 @@
 class Solution {
     public int solution(String[] order) {
         int answer = 0;
-        for(String str:order) {
-            if(str.contains("cafelatte")) {
-                answer+=5000;
-            } else {
-                answer+=4500;
-            }
+        for (String i : order) {
+            if (i.contains("cafelatte"))
+                answer += 5000;
+            else if (i.contains("americano"))
+                answer += 4500;
+            else
+                answer += 4500;
         }
         return answer;
     }

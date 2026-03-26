307a3da68c9347e24330fbad116c3297d4a48fb3
다인 <129330821+dainnida@users.noreply.github.com>
2026-03-27 00:09:25 +0900

    [level 2] Title: 모음 사전, Time: 8.85 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,14 +1,32 @@
-from collections import deque
+# from collections import deque
 
 def solution(word):
     answer = 0
-    dic = {'A': 0, 'E': 1, 'I': 2, 'O': 3, 'U': 4}
-    cnt = deque([1])
-    for i in range(1, 5):
-        cnt.appendleft(5*cnt[0]+1)
-    cnt = list(cnt)
+#     dic = {'A': 0, 'E': 1, 'I': 2, 'O': 3, 'U': 4}
+#     cnt = deque([1])
+#     for i in range(1, 5):
+#         cnt.appendleft(5*cnt[0]+1)
+#     cnt = list(cnt)
     
-    for idx, w in enumerate(word):
-        answer += dic[w] * cnt[idx] + 1
+#     for idx, w in enumerate(word):
+#         answer += dic[w] * cnt[idx] + 1
+    dic = []
+    found = False
     
-    return answer
+    def make(tmp):
+        nonlocal found
+        
+        if tmp ==  word:
+            found = True
+            return
+        if len(tmp) > 5:
+            return
+        if tmp:
+            dic.append(tmp)
+        for i in "AEIOU":
+            if found == True:
+                break
+            make(tmp+i)
+            
+    make("")
+    return len(dic)+1

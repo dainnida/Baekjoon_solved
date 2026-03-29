7086cd5dc34c2f2e7929be23d8743ed87e0d56f9
다인 <129330821+dainnida@users.noreply.github.com>
2026-03-29 22:35:48 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 93.36 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -10,8 +10,6 @@ def change(n, k):
 def isPrime(n):
     if n == 1:
         return False
-    if '0' in str(n):
-        return False
     for i in range(2, int(math.sqrt(n))+1):
         if n % i == 0:
             return False

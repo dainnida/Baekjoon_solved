cf4ada6cea2d5d9ab61602f8fe17550eed6a218e
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-03 23:51:56 +0900

    [level 2] Title: [3차] 압축, Time: 29.88 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/README.md
+++ 프로그래머스/2/17684. ［3차］ 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 압축 - 17684 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684?language=java) 
 
 ### 성능 요약
 
-메모리: 9.21 MB, 시간: 1.34 ms
+메모리: 89.1 MB, 시간: 29.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 02일 16:07:31
+2026년 04월 03일 23:51:52
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(String msg) {
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> dic = new HashMap<>();
+        for (int i=1; i<27; i++)
+            dic.put("" + (char)('A'+i-1), i); // 'A'+1은 int, char로 변환하면 'B'
+        
+        int n = 0;
+while (n < msg.length()) {
+            String tmp = "" + msg.charAt(n);
+            int nextN = n + 1;
+            
+            while (nextN < msg.length()) {
+                char nextChar = msg.charAt(nextN);
+                if (!dic.containsKey(tmp + nextChar)) {
+                    break; 
+                }
+                tmp += nextChar;
+                nextN++;
+            }
+            
+            answer.add(dic.get(tmp));
+            
+            if (nextN < msg.length()) {
+                dic.put(tmp + msg.charAt(nextN), dic.size() + 1);
+            }
+            
+            n += tmp.length();
+        }
+        
+        return answer;
+    }
+}

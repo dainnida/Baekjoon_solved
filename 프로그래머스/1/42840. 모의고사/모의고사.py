7b5f346ef300099700916c483d6a415318aef745
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-05 02:24:26 +0900

    [level 1] Title: 모의고사, Time: 3.30 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,18 +1,18 @@
 def solution(answers):
-    answer = []
+    correct = []
     cnt = [0, 0, 0]
     one, two, three = [1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    for i in range(len(answers)):
-        if one[i % 5] == answers[i]:
+    for idx, answer in enumerate(answers):
+        if one[idx % 5] == answer:
             cnt[0] += 1
-        if two[i % 8] == answers[i]:
+        if two[idx % 8] == answer:
             cnt[1] += 1
-        if three[i % 10] == answers[i]:
+        if three[idx % 10] == answer:
             cnt[2] += 1
             
     maxN = max(cnt)
     for i in range(3):
         if maxN == cnt[i]:
-            answer.append(i+1)
-    return answer
+            correct.append(i+1)
+    return correct

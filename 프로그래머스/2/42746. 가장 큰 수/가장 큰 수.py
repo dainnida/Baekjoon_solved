14c7c28f62df12dd4bed40644c6766aadb899aae
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-05 01:42:01 +0900

    [level 2] Title: 가장 큰 수, Time: 1507.47 ms, Memory: 25.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    answer = ''
+    numbers = list(map(str, numbers))
+    numbers.sort(key = lambda x : x*3, reverse = True)
+    
+    return str(int(''.join(numbers))) # [0,0] 이라면 답이 "00"이 아닌 "0"어야 함

4535609c936351340469578d242c85e7fe0c1682
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-06 23:01:49 +0900

    [level 2] Title: [3차] n진수 게임, Time: 44.65 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -0,0 +1,24 @@
+def change(n, q):
+    result = ''
+    if n == 0:
+        return '0'
+    while (n != 0):
+        if (n%q) >= 10:
+            result += chr(ord('A') + (n%q) -10)
+        else:
+            result += str(n % q)
+        n //= q
+    return result[::-1]
+
+def solution(n, t, m, p):
+    answer = ''
+    count = 0 # n진수로 바꾸어야 할 수
+    total = 1 # n진수로 바꾼 길이의 총 합
+    while len(answer) <= t:
+        strN = change(count, n)
+        for idx, ch in enumerate(strN):
+            if (total+idx) % m == p % m:
+                answer += ch
+        total += len(strN)
+        count += 1
+    return answer[:t]

463d9e61f12b2da59fb1eb58b1deb9c994222821
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-06 23:02:30 +0900

    [level 2] Title: [3차] n진수 게임, Time: 44.82 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -17,7 +17,7 @@ def solution(n, t, m, p):
     while len(answer) <= t:
         strN = change(count, n)
         for idx, ch in enumerate(strN):
-            if (total+idx) % m == p % m:
+            if (total+idx) % m == p % m: # m=p일 때를 위해
                 answer += ch
         total += len(strN)
         count += 1

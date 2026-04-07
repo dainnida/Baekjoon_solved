fac2bfa4fb6f1fb4df89f9216ed5054d26f45948
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-07 15:18:41 +0900

    [level 2] Title: 택배상자, Time: 228.72 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -2,25 +2,32 @@ from collections import deque
 
 def solution(order):
     answer = 0
-    order = deque(order)
+#    order = deque(order)
     assis = []
     
-    for box in range(1, len(order)+1):
-        if box == order[0]:
-            order.popleft()
-            answer += 1
-        else:
-            while(assis and order[0] == assis[-1]):
-                order.popleft()
-                assis.pop()
-                answer += 1
-            assis.append(box)
+#     for box in range(1, len(order)+1):
+#         if box == order[0]:
+#             order.popleft()
+#             answer += 1
+#         else:
+#             while(assis and order[0] == assis[-1]):
+#                 order.popleft()
+#                 assis.pop()
+#                 answer += 1
+#             assis.append(box)
     
-    for i in assis[::-1]:
-        if i == order[0]:
-            order.popleft()
+#     for i in assis[::-1]:
+#         if i == order[0]:
+#             order.popleft()
+#             answer += 1
+#         else:
+#             break
+
+    for box in range(1, len(order)+1):
+        assis.append(box)
+        
+        while assis and order[answer] == assis[-1]:
             answer += 1
-        else:
-            break
+            assis.pop()
             
     return answer

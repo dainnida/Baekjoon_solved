6c9e9d59d8008aa9a9ab9671cfd6b671b9bfbd0d
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-08 14:58:02 +0900

    [level 2] Title: 숫자 변환하기, Time: 310.85 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(x, y, n):
+    visited =  set()
+    q = deque([(x, 0)])
+    visited.add(x)
+    
+    while q:
+        curr, count = q.popleft()
+        if curr == y:
+            return count
+        for node in [curr+n, curr*2, curr*3]:
+            if node == y:
+                return count+1
+            elif node < y and node not in visited:
+                visited.add(node)
+                q.append((node, count+1))
+
+    return -1

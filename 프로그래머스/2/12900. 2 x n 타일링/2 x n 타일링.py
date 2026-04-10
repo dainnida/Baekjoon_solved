c706c90834ddef5e0aea3443c8d9ba51acd52861
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-10 17:05:49 +0900

    [level 2] Title: 2 x n 타일링, Time: 11.72 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,11 @@
+import math
+
+def solution(n):
+    dp = [0] * (n+1)
+    dp[1] = 1 # 세로만
+    dp[2] = 2 # 세로2 or 가로만
+    
+    for i in range(3, n+1):
+        dp[i] = (dp[i-2] + dp[i-1]) % 1000000007 # dp[i-2]에 세로 2개 붙이거나 가로 1개 붙이기 or dp[i-1]에 세로 1개 붙이기
+            
+    return dp[n]

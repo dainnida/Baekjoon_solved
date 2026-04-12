38390fcf29a69513948fa2baefbd71377a8d054a
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-12 22:21:46 +0900

    [level 2] Title: 큰 수 만들기, Time: 89.02 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,10 +1,10 @@
-def solution(number, k):
+def solution(number, k): 
     answer = []
-       
+    
     for num in number:
         while k > 0 and answer and answer[-1] < num:
             answer.pop()
             k -= 1
         answer.append(num)
-        
-    return ''.join(answer[:len(answer)-k])
+    
+    return ''.join(answer[:len(answer)-k]) # 더 제거할 수가 남았다면 맨 뒤를 자름 이미 내림차순으로 잘 정렬되어 있기 때문

2c0f73f7906fdd53e4634831b27173830009b689
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-20 16:41:39 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 4.38 ms, Memory: 9.52 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,19 @@
+def solution(files):
+    answer = []
+    dic = {}
+    for idx, file in enumerate(files):
+        for i, f in enumerate(file):
+            if f.isdecimal():
+                start = i
+                break
+        for i in range(start, min(len(file), start+5)):
+            if file[i].isdecimal():
+                fin = i
+            else:
+                break
+        head = file[:start].upper()
+        number = file[start:fin+1]
+        answer.append((file, head, number))
+
+    answer.sort(key = lambda x : (x[1], int(x[2])))
+    return [a[0] for a in answer]

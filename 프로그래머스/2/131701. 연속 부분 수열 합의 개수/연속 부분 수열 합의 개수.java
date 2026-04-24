e9106e0a5b3f384fb1a67a7d31ef5de9ad2ac034
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-25 02:18:52 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 106.78 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -5,13 +5,14 @@ class Solution {
         int len = elements.length;
         Set<Integer> set = new HashSet<>();
         int[][] dp = new int[len+1][len]; // 행: 부분 수열 길이, 열: 수열 시작 인덱스
-        int[] round = new int[len * 2 - 1];
-        for (int i = 0; i < len * 2 - 1; i++)
-            round[i] = elements[i % len];
-        // System.out.println(Arrays.toString(round));
+        // int[] round = new int[len * 2 - 1];
+        // for (int i = 0; i < len * 2 - 1; i++)
+        //     round[i] = elements[i % len];
+
         for (int i = 1; i <= len; i++) {
             for (int j = 0; j < len; j++) {
-                dp[i][j] = dp[i-1][j] + round[i+j-1];
+                // dp[i][j] = dp[i-1][j] + round[i+j-1];
+                dp[i][j] = dp[i-1][j] + elements[(i+j-1) % len];
                 set.add(dp[i][j]);
             }
         }

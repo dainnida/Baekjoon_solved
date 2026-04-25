91a8288657a2343aef9dc7ad89127e93786ad82a
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-25 14:16:35 +0900

    [level 3] Title: 거스름돈, Time: 15.92 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int n, int[] money) {
         int[] dp = new int[n+1];
+        dp[0] = 1;
         for (int m: money) {
-            dp[m] += 1;
-            for (int i = m + 1; i <= n; i++)
+            for (int i = m; i <= n; i++)
                 dp[i] = (dp[i] + dp[i-m]) % 1000000007;
         }
         return dp[n];

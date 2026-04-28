6028e5e01ce8c7ce93025ce57b8ec80f89e7d587
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-28 14:06:43 +0900

    [level 2] Title: 올바른 괄호, Time: 6.43 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,31 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Deque<String> stack = new ArrayDeque<>();
+//         Deque<String> stack = new ArrayDeque<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            if ((s.charAt(i)+"").equals("(")) 
-                stack.push("(");
+//         for (int i = 0; i < s.length(); i++) {
+//             if ((s.charAt(i)+"").equals("(")) 
+//                 stack.push("(");
+//             else {
+//                 if (stack.isEmpty() || (stack.peek()).equals(")"))
+//                     return false;
+//                 stack.pop();
+//             }
+//         }
+//         if (stack.isEmpty())
+//             return true;
+//         return false;
+        int count = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                count++;
             else {
-                if (stack.isEmpty() || (stack.peek()).equals(")"))
+                if (count == 0)
                     return false;
-                stack.pop();
+                count--;
             }
         }
-        if (stack.isEmpty())
-            return true;
-        return false;
+        return count == 0;
     }
 }

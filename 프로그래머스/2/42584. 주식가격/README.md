adf3dc2daf44dbe8d751d3d1b9ab539991d9da02
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-29 21:46:45 +0900

    [level 2] Title: 주식가격, Time: 20.63 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
 
 ### 성능 요약
 
-메모리: 18.8 MB, 시간: 36.13 ms
+메모리: 79.2 MB, 시간: 20.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 29일 21:28:12
+2026년 04월 29일 21:46:40
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            answer[i] = prices.length - i - 1;
+        }
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            stack.push(i);
+        }
+        
+        return answer;
+    }
+}

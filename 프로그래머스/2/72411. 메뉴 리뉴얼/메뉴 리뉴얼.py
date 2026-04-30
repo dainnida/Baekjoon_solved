ae46c8869aaf507c5e1293a2c0bcdfed26e53b6b
다인 <129330821+dainnida@users.noreply.github.com>
2026-04-30 18:08:09 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 2.09 ms, Memory: 9.75 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -10,11 +10,15 @@ def solution(orders, course):
             order_list = sorted(list(order))
             combi += list("".join(p) for p in combinations(order_list, num))
         combi_dic = dict(Counter(combi))
+        # key 기준 내림차순
+        combi_dic = dict(sorted(combi_dic.items(), key = lambda x : -x[1]))
         if(combi_dic):
             max_value = max(combi_dic.values())
             if max_value >= 2:
                 for key, value in combi_dic.items():
                     if value == max_value:
                         answer.append(key)
+                    if value < max_value:
+                        break
         
     return sorted(answer)

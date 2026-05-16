ff4a67b6ddc059205ff3f5fe0d7c76105f8e634f
다인 <129330821+dainnida@users.noreply.github.com>
2026-05-16 22:12:41 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.09 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,22 +1,17 @@
 class Solution {
     public int solution(int storey) {
-        int answer = 0;
-        int[] stone = new int[storey+1];
-        stone[0] = 0; stone[1] = 1;
+        return dfs(storey);
+    }
+    
+    private int dfs(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
         
-        for (int i = 2; i <= storey; i++) {
-            int minS = 1000000;
-            int j = 1;
-            while (i - j >= 0) {
-                minS = Math.min(minS, stone[i-j] + 1);
-                j *= 10;
-            }
-            if (j - i < i) { // 뺄셈인 경우도 보기
-                minS = Math.min(minS, stone[j-i] + 1);
-            }
-            stone[i] = minS;
-        }
+        int down = dfs(n / 10) + n % 10;
+        int up = dfs(n / 10 + 1) + (10 - n % 10);
         
-        return stone[storey];
+        return Math.min(down, up);
     }
 }

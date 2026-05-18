834f1c2b6c1a8bfd2cadede13855f11b30d670f2
다인 <129330821+dainnida@users.noreply.github.com>
2026-05-18 23:02:35 +0900

    [level 2] Title: 배달, Time: 2.81 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -4,16 +4,20 @@ class Solution {
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
         
+        // 마을 1부터 N까지의 최단 거리
         int[] distance = new int[N+1];
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[1] = 0;
         
+        // 인접한 마을을 잇는 그래프 초기화
         List<List<int[]>> graph = new ArrayList<>();
         for (int i = 0; i <= N; i++)
             graph.add(new ArrayList<>());
         
+        // [다음 노드, 다음 노드까지의 길이]가 저장된 우선순위 큐
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
+        // 인접한 마을 잇기
         for (int[] r : road) {
             int a = r[0], b = r[1], c = r[2];
             graph.get(a).add(new int[]{b, c});
@@ -38,6 +42,7 @@ class Solution {
             }            
         }
         
+        // K보다 작은 거리 카운트
         for (int i = 1; i <= N; i++) {
             if (distance[i] <= K)
                 answer++;

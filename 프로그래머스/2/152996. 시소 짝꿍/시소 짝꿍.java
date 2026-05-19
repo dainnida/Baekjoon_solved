5bb4cf20190298269e114fa16f8553a1b2441a62
다인 <129330821+dainnida@users.noreply.github.com>
2026-05-19 23:43:09 +0900

    [level 2] Title: 시소 짝꿍, Time: 14.78 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+        Map<Integer, Long> dic = new HashMap<>();
+        
+        for (int weight : weights) 
+            dic.put(weight, dic.getOrDefault(weight, 0L) + 1);   
+        
+        for (int weight : dic.keySet()) {
+            long value = dic.get(weight);
+            
+            // 1 : 1
+            if (value > 1)
+                answer += value * (value - 1) / 2;
+            
+            // 2 : 3
+            if (weight % 2 == 0 && dic.containsKey(weight / 2 * 3))
+                answer += value * dic.get(weight / 2 * 3);
+            
+            // 2 : 4
+            if (dic.containsKey(weight * 2))
+                answer += value * dic.get(weight * 2);
+            
+            // 3 : 4
+            if (weight % 3 == 0 && dic.containsKey(weight / 3 * 4))
+                answer += value * dic.get(weight / 3 * 4);
+        }
+        
+        return answer;
+    }
+}

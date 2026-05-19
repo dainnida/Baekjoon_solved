896153508e3662fc9f57f0902540a2d5c7928038
다인 <129330821+dainnida@users.noreply.github.com>
2026-05-19 23:46:25 +0900

    [level 2] Title: 시소 짝꿍, Time: 12.83 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -9,23 +9,23 @@ class Solution {
             dic.put(weight, dic.getOrDefault(weight, 0L) + 1);   
         
         for (int weight : dic.keySet()) {
-            long value = dic.get(weight);
+            long count = dic.get(weight);
             
             // 같은 몸무게
-            if (value > 1)
-                answer += value * (value - 1) / 2;
+            if (count > 1)
+                answer += count * (count - 1) / 2;
             
             // 2 : 3
             if (weight % 2 == 0 && dic.containsKey(weight / 2 * 3))
-                answer += value * dic.get(weight / 2 * 3);
+                answer += count * dic.get(weight / 2 * 3);
             
             // 2 : 4
             if (dic.containsKey(weight * 2))
-                answer += value * dic.get(weight * 2);
+                answer += count * dic.get(weight * 2);
             
             // 3 : 4
             if (weight % 3 == 0 && dic.containsKey(weight / 3 * 4))
-                answer += value * dic.get(weight / 3 * 4);
+                answer += count * dic.get(weight / 3 * 4);
         }
         
         return answer;

96ef6307869770e168a60a0d944df9746dc830cd
다인 <129330821+dainnida@users.noreply.github.com>
2026-05-22 17:34:39 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 12.46 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -2,6 +2,8 @@ class Solution {
     public int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
         int[][] board = new int[rows+1][columns+1];
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
         
         // 행렬 만들기
         for (int i = 1; i <= rows; i++) {
@@ -12,39 +14,30 @@ class Solution {
         // 회전하기
         for (int i = 0; i < queries.length; i++) {
             int x1 = Math.min(queries[i][0], queries[i][2]), y1 = Math.min(queries[i][1], queries[i][3]), x2 = Math.max(queries[i][0], queries[i][2]), y2 = Math.max(queries[i][1], queries[i][3]);
-            int min = 10001;
             
             int previous = board[x1][y1];
-            min = Math.min(min, previous);
-            for (int j = y1 + 1; j <= y2; j++) {
-                int temp = board[x1][j];
-                board[x1][j] = previous;
-                previous = temp;
-                min = Math.min(min, previous);
-            }
+            int min = previous;
             
-            for (int j = x1 + 1; j <= x2; j++) {
-                int temp = board[j][y2];
-                board[j][y2] = previous;
-                previous = temp;
-                min = Math.min(min, previous);
-            }
+            int cx = x1, cy = y1;
+            int dir = 0;
             
-            for (int j = y2 - 1; j >= y1; j--) {
-                int temp = board[x2][j];
-                board[x2][j] = previous;
-                previous = temp;
-                min = Math.min(min, previous);
-            }
-            
-            for (int j = x2 - 1; j > x1; j--) {
-                int temp = board[j][y1];
-                board[j][y1] = previous;
-                previous = temp;
-                min = Math.min(min, previous);
-            }
+            while (dir < 4) {
+                int nx = cx + dx[dir];
+                int ny = cy + dy[dir];
                 
-            board[x1][y1] = previous;
+                if (nx < x1 || nx > x2 || ny < y1 || ny > y2) {
+                    dir++;
+                    continue;
+                }
+            
+                int temp = board[nx][ny];
+                board[nx][ny] = previous;
+                previous = temp;
+                min = Math.min(min, previous);
+
+                cx = nx;
+                cy = ny;
+            }
             
             answer[i] = min;
         }

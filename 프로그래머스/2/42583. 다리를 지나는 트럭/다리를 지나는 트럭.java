79b9c73c7771782a905f910b80592506f4a7f84b
다인 <129330821+dainnida@users.noreply.github.com>
2026-05-25 22:46:22 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 10.58 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,34 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 1;
-        Deque<int[]> bridge = new ArrayDeque<>();
+        int answer = 0;
+        Deque<Integer> bridge = new ArrayDeque<>();
         int total_weight = 0;
+        int index = 0;
         
-        for (int truck_weight : truck_weights) {
-            while (!bridge.isEmpty()) {
-                // 현재 트럭이 다리에 오를 수 있을 때까지
-                if (total_weight + truck_weight <= weight && bridge.size() + 1 <= bridge_length)
-                    break;
-                
-                int[] first = bridge.poll();
-                total_weight -= first[0];
-                // 해당 트럭이 다리를 다 지나가는 데 걸리는 시간
-                if (answer - first[1] < bridge_length) // 아직 더 지나가야 함
-                    answer += bridge_length - (answer - first[1]);
-            }
+        while (index < truck_weights.length) {
+            if (bridge.size() == bridge_length)
+                total_weight -= bridge.poll();
             
-            bridge.offer(new int[]{truck_weight, answer});
-            total_weight += truck_weight;
+            // 현재 트럭이 다리에 오를 수 있다면
+            if (total_weight + truck_weights[index] <= weight) {
+                bridge.offer(truck_weights[index]);
+                total_weight += truck_weights[index];
+                index++;
+            }
+            else // 오를 수 없다면 빈 공간으로 채움
+                bridge.offer(0);
+           
             answer++;
         }
-        // 남은 트럭 다 지나가기
-        while (!bridge.isEmpty()) {
-            int[] first = bridge.poll();
-            total_weight -= first[0];
-            answer += bridge_length - (answer - first[1]);
-        }
 
-        return answer;
+        return answer + bridge_length;
     }
 }

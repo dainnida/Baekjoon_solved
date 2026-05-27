db0016ab9b67b483c104cbf67e5a475a1ef9e482
다인 <129330821+dainnida@users.noreply.github.com>
2026-05-27 16:36:48 +0900

    [level 3] Title: 야근 지수, Time: 6.05 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -3,21 +3,6 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-//         Map<Integer, Integer> workCount = new TreeMap<>();
-        
-//         for (int i = 0; i < works.length; i++)
-//             workCount.put(works[i], workCount.getOrDefault(works[i], 0)+ 1);
-        
-//         List<Integer> workKey = new ArrayList<>(workCount.getKeys());
-//         int index = 0;
-//         while (n > 0) {
-//             if (index >= works.length)
-//                 return 0;
-//             if (index == works.length - 1) {
-                
-//             }
-//             int gap = work
-//         }
         
         Arrays.sort(works);
         int index = works.length - 1;

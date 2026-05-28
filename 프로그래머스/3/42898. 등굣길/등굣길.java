360763a7d2e8370c67007b0203443cf7ff8b7d60
다인 <129330821+dainnida@users.noreply.github.com>
2026-05-28 23:37:44 +0900

    [level 3] Title: 등굣길, Time: 0.39 ms, Memory: 55.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+        int[][] dp = new int[n+1][m+1];
+        boolean[][] water = new boolean[n+1][m+1];
+        
+        if (puddles.length > 0) {
+            for (int[] puddle : puddles)
+                water[puddle[1]][puddle[0]] = true;   
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (water[i][j])
+                    continue;
+                if (i == 1 && j == 1)
+                    dp[i][j] = 1;
+                else if (i == 1 && j > 1)
+                    dp[i][j] = dp[i][j-1];
+                else if (i > 1 && j == 1)
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1000000007;
+            }
+        }
+        
+        return dp[n][m];
+    }
+}

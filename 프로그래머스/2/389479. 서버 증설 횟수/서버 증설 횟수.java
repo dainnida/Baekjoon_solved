c9bcb1a322fb88de6364826508f3a749c74244c1
다인 <129330821+dainnida@users.noreply.github.com>
2026-05-30 01:41:17 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -3,25 +3,22 @@ import java.util.*;
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
-        Deque<Integer> q = new ArrayDeque<>();
+        int[] minusServer = new int[24 + k];
+        int server = 0;
         
         for (int i = 0; i < 24; i++) {
-            int num = players[i];
             
             // 시간 지난 서버 반납
-            while (!q.isEmpty() && q.peek() + k == i) {
-                q.poll();
-            }
+            server -= minusServer[i];
             
             // 서버 증설 필요한지 계산하기
-            int server = q.size();
+            int requireServer = players[i] / m;
             // 서버 증설 필요
-            if (num >= (server + 1) * m) {
-                // int count = (num - (server * m)) / m;
-                int count = num / m - server;
+            if (requireServer > server) {
+                int count = requireServer - server;
                 answer += count;
-                for (int j = 0; j < count; j++)
-                    q.offer(i);
+                server += count;
+                minusServer[i + k] += count;
             }
         }
         

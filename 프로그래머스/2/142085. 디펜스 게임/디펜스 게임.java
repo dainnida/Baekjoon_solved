51bac838f55f4eb21a40c7b582791ddd1334c4f2
다인 <129330821+dainnida@users.noreply.github.com>
2026-05-30 16:54:48 +0900

    [level 2] Title: 디펜스 게임, Time: 64.43 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i = 0; i < enemy.length; i++) {
+            
+            // 무족권이 없다고 생각하고 적이랑 싸운다
+            n -= enemy[i];
+            pq.offer(enemy[i]);
+            
+            // 적과 더이상 싸울 수 없다면, 가장 많은 적과 싸웠을 때 무족권을 쓴다
+            if (n < 0) {
+                if (k > 0) {
+                    n += pq.poll();
+                    k--;
+                }
+                else
+                    return answer;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}

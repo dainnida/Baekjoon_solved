b735c0d478f75a5ff5b27736e3f31c290b380f38
다인 <129330821+dainnida@users.noreply.github.com>
2026-06-09 21:39:05 +0900

    [level 2] Title: 요격 시스템, Time: 301.84 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -4,19 +4,37 @@ class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
         
-        Arrays.sort(targets, (a, b) -> {
-            if (b[0] == a[0])
-                return b[1] - a[1];
+        // 1. 내림차순으로 정렬하는 방법
+//         Arrays.sort(targets, (a, b) -> {
+//             if (b[0] == a[0])
+//                 return b[1] - a[1];
             
-            return b[0] - a[0];
+//             return b[0] - a[0];
+//         });
+        
+//         int bullet = targets[0][0]; // 실수라 해당 수보다 아주 조금 작다고 치자.
+//         answer++;
+//         for (int i = 1; i < targets.length; i++) {
+//             if (targets[i][1] <= bullet) { // 범위 안에 들어가지 못한다.
+//                 answer++;
+//                 bullet = targets[i][0];
+//             }
+//         }
+        
+        // 2. 오름차순으로 정렬하는 방법
+        Arrays.sort(targets, (a, b) -> {
+            if (a[1] == b[1])
+                return a[0] - b[0];
+            
+            return a[1] - b[1];
         });
         
-        int bullet = targets[0][0]; // 실수라 해당 수보다 아주 조금 작다고 치자.
+        int bullet = targets[0][1]; // 실수라 해당 수보다 아주 조금 작다고 치자.
         answer++;
         for (int i = 1; i < targets.length; i++) {
-            if (targets[i][1] <= bullet) { // 범위 안에 들어가지 못한다.
+            if (targets[i][0] >= bullet) { // 범위 안에 들어가지 못한다.
                 answer++;
-                bullet = targets[i][0];
+                bullet = targets[i][1];
             }
         }
                

60f35e85abf4e91335aecd010bfc9ebc94b87e3e
다인 <129330821+dainnida@users.noreply.github.com>
2026-06-28 17:56:56 +0900

    [level 3] Title: 단속카메라, Time: 5.39 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -4,23 +4,35 @@ class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
         
-        Arrays.sort(routes, (a, b) -> {
-            if (a[0] == b[0])
-                return a[1] - b[1];
-            return a[0] - b[0];
-        });
+//         Arrays.sort(routes, (a, b) -> {
+//             if (a[0] == b[0])
+//                 return a[1] - b[1];
+//             return a[0] - b[0];
+//         });
         
-        int start = routes[0][0], end = routes[0][1];
+//         int start = routes[0][0], end = routes[0][1];
+//         answer++;
+//         for (int i = 1; i < routes.length; i++) {
+//             if (routes[i][0] > end) { // 겹치는 부위X
+//                 start = routes[i][0];
+//                 end = routes[i][1];
+//                 answer++;
+//             }
+//             else {
+//                 start = routes[i][0];
+//                 end = Math.min(end, routes[i][1]);
+//             }
+//         }
+        
+        // end만 봐도 됨 기준
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        
+        int end = routes[0][1];
         answer++;
         for (int i = 1; i < routes.length; i++) {
-            if (routes[i][0] > end) { // 겹치는 부위X
-                start = routes[i][0];
+            if (routes[i][0] > end) {
                 end = routes[i][1];
                 answer++;
-            }
-            else {
-                start = routes[i][0];
-                end = Math.min(end, routes[i][1]);
             }
         }
     
